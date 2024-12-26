cae5c20e2fc08bce17477959dd5cee1db7b0f55d
Jay200123 <kenken.ayonon@gmail.com>
2024-12-26 20:57:15 +0800

    Add MainLayout component to structure the application with Navbar and Footer

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,16 @@
+import { Navbar, Footer } from "../components";
+import { Outlet } from "react-router-dom";
+
+export default function () {
+  return (
+    <>
+      <main className="flex flex-col justify-between  min-h-screen min-w-screen">
+        <Navbar />
+        <div>
+          <Outlet />
+        </div>
+        <Footer />
+      </main>
+    </>
+  );
+}
