3bc02258ce56d26bfabecc4795d67649d34086cb
Jay200123 <kenken.ayonon@gmail.com>
2024-12-27 17:58:33 +0800

    Improve MainLayout spacing and alignment for better layout consistency

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -4,9 +4,9 @@ import { Outlet } from "react-router-dom";
 export default function () {
   return (
     <>
-      <main className="flex flex-col justify-between  min-h-screen min-w-screen">
+      <main className="flex flex-col justify-between min-h-screen min-w-screen">
         <Navbar />
-        <div>
+        <div className="m-1">
           <Outlet />
         </div>
         <Footer />
