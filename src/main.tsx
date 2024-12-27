dbad6f95e029eb7f10bc7d978a00fe187dcbe50d
Jay200123 <kenken.ayonon@gmail.com>
2024-12-27 17:57:31 +0800

    Wrap App component with ThemeProvider for enhanced theming support

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { ToastContainer } from 'react-toastify'
-import './index.css'
-import App from './App'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { ToastContainer } from "react-toastify";
+import "./index.css";
+import App from "./App";
+import { ThemeProvider } from "@material-tailwind/react";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ToastContainer/>
-    <App />
-  </StrictMode>,
-)
+    <ToastContainer />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </StrictMode>
+);
