06a162ad32c8cc96a0a9b0079a304aa03541d324
Jay200123 <kenken.ayonon@gmail.com>
2024-12-27 17:58:28 +0800

    Refactor Navbar layout for improved alignment and spacing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,12 +5,14 @@ export default function () {
         <i className="fa-solid fa-bag-shopping text-lg m-2"></i>
         <h3 className="text-lg font-bold"> IT Shop</h3>
       </div>
-      <ul className="flex items-center p-1">
-        <li className="text-sm m-2 font-semibold">Home</li>
-        <li className="text-sm m-2 font-semibold">Shop</li>
-        <li className="text-sm m-2 font-semibold">Product</li>
-        <li className="text-sm m-2 font-semibold">Contact Us</li>
-      </ul>
+      <div className="flex items-center justify-center">
+        <ul className="flex flex-row items-start justify-start">
+          <li className="text-sm m-3 font-semibold">Home</li>
+          <li className="text-sm m-3 font-semibold">Shop</li>
+          <li className="text-sm m-3 font-semibold">Product</li>
+          <li className="text-sm m-3 font-semibold">Contact Us</li>
+        </ul>
+      </div>
       <div className="p-1">
         <i className="fa-solid fa-magnifying-glass text-lg m-2"></i>
         <i className="fa-regular fa-circle-user text-lg m-2"></i>
