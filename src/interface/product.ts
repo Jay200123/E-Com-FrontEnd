bfe11736569d853161daaf7eaa7495e1d3ec155c
Jay200123 <kenken.ayonon@gmail.com>
2024-12-27 21:55:48 +0800

    Add Product interface for type safety in product-related data

---
+++ src/interface/product.ts
@@ -0,0 +1,25 @@
+import { Brand } from "./brand";
+import { Image } from "./image";
+
+interface Product {
+  _id: string;
+  brand: Brand;
+  product_name: string;
+  price: number;
+  description: string;
+  color: string;
+  quantity: number;
+  image: Image[];
+}
+
+interface ProductState {
+  loading: boolean;
+  message: string;
+  getAllProducts: () => Promise<Product[]>;
+  getProductById: (id: string) => Promise<Product>;
+  addProduct: (data: FormData) => Promise<void>;
+  updateProduct: (id: string, data: FormData) => Promise<void>;
+  deleteProduct: (id: string) => Promise<void>;
+}
+
+export type { Product, ProductState };
