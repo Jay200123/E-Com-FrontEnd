389297903032b8c91bd60c8bf36c2c4b558df84c
Jay200123 <kenken.ayonon@gmail.com>
2024-12-28 19:31:41 +0800

    Add Profile component with layout for user image and profile info

---
+++ src/pages/Profile/index.tsx
@@ -0,0 +1,10 @@
+export default function(){
+    return (
+        <>
+        <div className="flex items-center w-full h-screen">
+            <div className="w-1/2 h-full border border-black">User Image</div>
+            <div className="w-1/2 h-full border border-black"> Profile Info</div>
+        </div>
+        </>
+    )
+}
