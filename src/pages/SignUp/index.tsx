e1240890204d012810c0f46adeae549a8bcab793
Jay200123 <kenken.ayonon@gmail.com>
2024-12-28 19:13:20 +0800

    Update SignUp component to adjust form height and add profile picture upload

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -2,7 +2,7 @@ import ImageOne from "../../assets/signUp.avif";
 
 export default function () {
   return (
-    <form className="flex items-center w-full h-[32rem] rounded-sm shadow-md">
+    <form className="flex items-center w-full h-[36rem] rounded-sm shadow-md">
       <div className="w-1/2 h-full">
         <img
           src={ImageOne}
@@ -12,7 +12,10 @@ export default function () {
       </div>
       <div className="flex flex-col w-1/2 h-full p-4">
         <h3 className="mb-2 text-3xl font-semibold">Sign Up</h3>
-        <p className="mb-4 text-[1rem] text-gray-600">Already have an account?<span className="ml-1 font-bold underline">Sign In</span></p>
+        <p className="mb-4 text-[1rem] text-gray-600">
+          Already have an account?
+          <span className="ml-1 font-bold underline">Sign In</span>
+        </p>
         <input
           type="text"
           placeholder="Full Name"
@@ -36,6 +39,12 @@ export default function () {
         <input
           type="password"
           placeholder="Password"
+          className="p-1 mb-4 text-[1rem] border-b border-gray-700 rounded-sm"
+        />
+
+        <input
+          type="file"
+          placeholder="Upload Profile Picture"
           className="p-1 mb-4 text-[1rem] border-b border-gray-700 rounded-sm"
         />
         <div className="flex items-center mb-4 space-x-2">
