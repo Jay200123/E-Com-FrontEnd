970ae9cacae4e2f1e9d509b41721d7a4143c9aea
Jay200123 <kenken.ayonon@gmail.com>
2024-12-28 14:00:50 +0800

    Add routing for authentication and product categories in App component

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,19 @@ import {
   RouterProvider,
 } from "react-router-dom";
 
-import { Home, Product } from "./pages";
+import {
+  Home,
+  SignIn,
+  SignUp,
+  MobileProducts,
+  ComputerProducts,
+  LaptopProducts,
+} from "./pages";
 
-import { MainLayout } from "./layouts";
+import { 
+  MainLayout,
+  ProductLayout
+} from "./layouts";
 
 export default function App() {
   const Router = createBrowserRouter(
@@ -16,7 +26,14 @@ export default function App() {
         {/* public routes */}
         <Route element={<MainLayout />}>
           <Route path="/" element={<Home />} />
-          <Route path="/product" element={<Product />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Route>
+
+        <Route element={<ProductLayout />}>
+          <Route path="/products/mobiles" element={<MobileProducts />} />
+          <Route path="/products/laptops" element={<LaptopProducts />} />
+          <Route path="/products/computers" element={<ComputerProducts />} />
         </Route>
       </Route>
     )
