ca4164ca75ac76f65fe9a42469af5de8edddc6b4
Jay200123 <kenken.ayonon@gmail.com>
2024-12-28 19:13:38 +0800

    Update ComputerProduct component to enhance layout and display product cards

--- src/pages/Products/ComputerProduct.tsx
+++ src/pages/Products/ComputerProduct.tsx
@@ -1,8 +1,81 @@
-export default function(){
-    return (
-        <>
-            <h1>Computer Products</h1>
-            <p>Here are the Computer products</p>
-        </>
-    )
+import ImageOne from "../../assets/pc-1.jpg";
+
+export default function () {
+  return (
+    <>
+       <div className="relative flex flex-col items-center w-full h-screen p-10 space-x-4 overflow-x-auto md:grid md:grid-cols-4 md:gap-4">
+       <h3 className="absolute text-2xl font-bold left-1 top-1"><span>Desktops</span></h3>
+        <div className="min-w-[15rem] overflow-hidden max-w-[18rem] cursor-pointer transition-all duration-500 h-auto p-2 flex-shrink-0 border border-gray-300 rounded-md">
+          <img src={ImageOne} className="object-contain w-full h-64" />
+          <p className="font-semibold">Dell IntelCore i5 Complete PC Set</p>
+          <div className="flex items-center justify-between">
+            <p className="font-semibold">₱6,490.00</p>
+            <i className="text-2xl transition-all duration-500 cursor-pointer fa-solid hover:text-red-500 fa-cart-shopping"></i>
+          </div>
+        </div>
+
+        <div className="min-w-[15rem] max-w-[18rem] cursor-pointer transition-all duration-500 h-auto p-2 flex-shrink-0 border border-gray-300 rounded-md">
+          <img src={ImageOne} className="object-contain w-full h-64" />
+          <p className="font-semibold">Dell IntelCore i5 Complete PC Set</p>
+          <div className="flex items-center justify-between">
+            <p className="font-semibold">₱6,490.00</p>
+            <i className="text-2xl transition-all duration-500 cursor-pointer fa-solid hover:text-red-500 fa-cart-shopping"></i>
+          </div>
+        </div>
+
+        <div className="min-w-[15rem] max-w-[18rem] cursor-pointer transition-all duration-500 h-auto p-2 flex-shrink-0 border border-gray-300 rounded-md">
+          <img src={ImageOne} className="object-contain w-full h-64" />
+          <p className="font-semibold">Dell IntelCore i5 Complete PC Set</p>
+          <div className="flex items-center justify-between">
+            <p className="font-semibold">₱6,490.00</p>
+            <i className="text-2xl transition-all duration-500 cursor-pointer fa-solid hover:text-red-500 fa-cart-shopping"></i>
+          </div>
+        </div>
+        <div className="min-w-[15rem] max-w-[18rem] cursor-pointer transition-all duration-500 h-auto p-2 flex-shrink-0 border border-gray-300 rounded-md">
+          <img src={ImageOne} className="object-contain w-full h-64" />
+          <p className="font-semibold">Dell IntelCore i5 Complete PC Set</p>
+          <div className="flex items-center justify-between">
+            <p className="font-semibold">₱6,490.00</p>
+            <i className="text-2xl transition-all duration-500 cursor-pointer fa-solid hover:text-red-500 fa-cart-shopping"></i>
+          </div>
+        </div>
+
+        <div className="min-w-[15rem] max-w-[18rem] cursor-pointer transition-all duration-500 h-auto p-2 flex-shrink-0 border border-gray-300 rounded-md">
+          <img src={ImageOne} className="object-contain w-full h-64" />
+          <p className="font-semibold">Dell IntelCore i5 Complete PC Set</p>
+          <div className="flex items-center justify-between">
+            <p className="font-semibold">₱6,490.00</p>
+            <i className="text-2xl transition-all duration-500 cursor-pointer fa-solid hover:text-red-500 fa-cart-shopping"></i>
+          </div>
+        </div>
+
+        <div className="min-w-[15rem] max-w-[18rem] cursor-pointer transition-all duration-500 h-auto p-2 flex-shrink-0 border border-gray-300 rounded-md">
+          <img src={ImageOne} className="object-contain w-full h-64" />
+          <p className="font-semibold">Dell IntelCore i5 Complete PC Set</p>
+          <div className="flex items-center justify-between">
+            <p className="font-semibold">₱6,490.00</p>
+            <i className="text-2xl transition-all duration-500 cursor-pointer fa-solid hover:text-red-500 fa-cart-shopping"></i>
+          </div>
+        </div>
+
+        <div className="min-w-[15rem] max-w-[18rem] cursor-pointer transition-all duration-500 h-auto p-2 flex-shrink-0 border border-gray-300 rounded-md">
+          <img src={ImageOne} className="object-contain w-full h-64" />
+          <p className="font-semibold">Dell IntelCore i5 Complete PC Set</p>
+          <div className="flex items-center justify-between">
+            <p className="font-semibold">₱6,490.00</p>
+            <i className="text-2xl transition-all duration-500 cursor-pointer fa-solid hover:text-red-500 fa-cart-shopping"></i>
+          </div>
+        </div>
+
+        <div className="min-w-[15rem] max-w-[18rem] cursor-pointer transition-all duration-500 h-auto p-2 flex-shrink-0 border border-gray-300 rounded-md">
+          <img src={ImageOne} className="object-contain w-full h-64" />
+          <p className="font-semibold">Dell IntelCore i5 Complete PC Set</p>
+          <div className="flex items-center justify-between">
+            <p className="font-semibold">₱6,490.00</p>
+            <i className="text-2xl transition-all duration-500 cursor-pointer fa-solid hover:text-red-500 fa-cart-shopping"></i>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
