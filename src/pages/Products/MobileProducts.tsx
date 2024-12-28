57e4298db4dd61aa603efa985fe259aa6fe051fe
Jay200123 <kenken.ayonon@gmail.com>
2024-12-28 14:03:15 +0800

    Add MobileProducts component for displaying mobile products

---
+++ src/pages/Products/MobileProducts.tsx
@@ -0,0 +1,84 @@
+import ImageOne from "../../assets/mobile-3jpeg.jpg";
+
+export default function () {
+  return (
+    <>
+      <div className="flex items-center w-full h-screen p-4 space-x-4 overflow-x-auto md:grid md:grid-cols-4 md:gap-4">
+        <div className="min-w-[15rem] max-w-[18rem] cursor-pointer transition-all duration-500 h-auto p-2 flex-shrink-0 border border-gray-300 rounded-md">
+          <img src={ImageOne} className="object-cover w-full h-64" />
+          <p className="font-semibold">Samsung A05s</p>
+          <div className="flex items-center justify-between">
+            <p className="font-semibold">₱6,490.00</p>
+            <i className="text-2xl transition-all duration-500 cursor-pointer fa-solid hover:text-red-500 fa-cart-shopping"></i>
+          </div>
+        </div>
+
+        <div className="min-w-[15rem] max-w-[18rem] cursor-pointer transition-all duration-500 h-auto p-2 flex-shrink-0 border border-gray-300 rounded-md">
+          <img src={ImageOne} className="object-cover w-full h-64" />
+          <p className="font-semibold">Samsung A05s</p>
+          <div className="flex items-center justify-between">
+            <p className="font-semibold">₱6,490.00</p>
+            <i className="text-2xl transition-all duration-500 cursor-pointer fa-solid hover:text-red-500 fa-cart-shopping"></i>
+          </div>
+        </div>
+
+        <div className="min-w-[15rem] max-w-[18rem] cursor-pointer transition-all duration-500 h-auto p-2 flex-shrink-0 border border-gray-300 rounded-md">
+          <img src={ImageOne} className="object-cover w-full h-64" />
+          <p className="font-semibold">Samsung A05s</p>
+          <div className="flex items-center justify-between">
+            <p className="font-semibold">₱6,490.00</p>
+            <i className="text-2xl transition-all duration-500 cursor-pointer fa-solid hover:text-red-500 fa-cart-shopping"></i>
+          </div>
+        </div>
+
+        <div className="min-w-[15rem] max-w-[18rem] cursor-pointer transition-all duration-500 h-auto p-2 flex-shrink-0 border border-gray-300 rounded-md">
+          <img src={ImageOne} className="object-cover w-full h-64" />
+          <p className="font-semibold">Samsung A05s</p>
+          <div className="flex items-center justify-between">
+            <p className="font-semibold">₱6,490.00</p>
+            <i className="text-2xl transition-all duration-500 cursor-pointer fa-solid hover:text-red-500 fa-cart-shopping"></i>
+          </div>
+        </div>
+
+        <div className="min-w-[15rem] max-w-[18rem] cursor-pointer transition-all duration-500 h-auto p-2 flex-shrink-0 border border-gray-300 rounded-md">
+          <img src={ImageOne} className="object-cover w-full h-64" />
+          <p className="font-semibold">Samsung A05s</p>
+          <div className="flex items-center justify-between">
+            <p className="font-semibold">₱6,490.00</p>
+            <i className="text-2xl transition-all duration-500 cursor-pointer fa-solid hover:text-red-500 fa-cart-shopping"></i>
+          </div>
+        </div>
+
+        <div className="min-w-[15rem] max-w-[18rem] cursor-pointer transition-all duration-500 h-auto p-2 flex-shrink-0 border border-gray-300 rounded-md">
+          <img src={ImageOne} className="object-cover w-full h-64" />
+          <p className="font-semibold">Samsung A05s</p>
+          <div className="flex items-center justify-between">
+            <p className="font-semibold">₱6,490.00</p>
+            <i className="text-2xl transition-all duration-500 cursor-pointer fa-solid hover:text-red-500 fa-cart-shopping"></i>
+          </div>
+        </div>
+
+        <div className="min-w-[15rem] max-w-[18rem] cursor-pointer transition-all duration-500 h-auto p-2 flex-shrink-0 border border-gray-300 rounded-md">
+          <img src={ImageOne} className="object-cover w-full h-64" />
+          <p className="font-semibold">Samsung A05s</p>
+          <div className="flex items-center justify-between">
+            <p className="font-semibold">₱6,490.00</p>
+            <i className="text-2xl transition-all duration-500 cursor-pointer fa-solid hover:text-red-500 fa-cart-shopping"></i>
+          </div>
+        </div>
+
+        <div className="min-w-[15rem] max-w-[18rem] cursor-pointer transition-all duration-500 h-auto p-2 flex-shrink-0 border border-gray-300 rounded-md">
+          <img src={ImageOne} className="object-cover w-full h-64" />
+          <p className="font-semibold">Samsung A05s</p>
+          <div className="flex items-center justify-between">
+            <p className="font-semibold">₱6,490.00</p>
+            <i className="text-2xl transition-all duration-500 cursor-pointer fa-solid hover:text-red-500 fa-cart-shopping"></i>
+          </div>
+        </div>
+        
+        
+               
+      </div>
+    </>
+  );
+}
