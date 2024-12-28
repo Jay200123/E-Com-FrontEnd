68670c0ef42e78e9c502ba1bfd8064ef086879c8
Jay200123 <kenken.ayonon@gmail.com>
2024-12-28 14:04:22 +0800

    Update ProductSidebar component to improve layout and styling

--- src/components/Sidebar/ProductSidebar.tsx
+++ src/components/Sidebar/ProductSidebar.tsx
@@ -3,7 +3,7 @@ export default function () {
     <div className="w-[10rem] h-screen bg-gray-700 text-white flex flex-col justify-between items-center">
       <h3 className="text-3xl font-bold text-center">IT Shop</h3>
 
-      <ul className="flex flex-col space-y-4 ">
+      <ul className="flex flex-col w-full space-y-4">
         <li className="text-left">Price</li>
         <li className="text-left">Brand</li>
         <li className="text-left">Features</li>
