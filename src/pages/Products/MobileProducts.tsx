56e9931e9fcd3a8d79e088dbcda888af056af0e9
Jay200123 <kenken.ayonon@gmail.com>
2024-12-28 19:13:25 +0800

    Update MobileProducts component to enhance layout and add title

--- src/pages/Products/MobileProducts.tsx
+++ src/pages/Products/MobileProducts.tsx
@@ -3,7 +3,8 @@ import ImageOne from "../../assets/mobile-3jpeg.jpg";
 export default function () {
   return (
     <>
-      <div className="flex items-center w-full h-screen p-4 space-x-4 overflow-x-auto md:grid md:grid-cols-4 md:gap-4">
+      <div className="relative flex flex-col items-center w-full h-screen p-10 space-x-4 overflow-x-auto md:grid md:grid-cols-4 md:gap-4">
+        <h3 className="absolute text-2xl font-bold left-1 top-1">Mobile Phones</h3>
         <div className="min-w-[15rem] max-w-[18rem] cursor-pointer transition-all duration-500 h-auto p-2 flex-shrink-0 border border-gray-300 rounded-md">
           <img src={ImageOne} className="object-cover w-full h-64" />
           <p className="font-semibold">Samsung A05s</p>
@@ -75,9 +76,6 @@ export default function () {
             <i className="text-2xl transition-all duration-500 cursor-pointer fa-solid hover:text-red-500 fa-cart-shopping"></i>
           </div>
         </div>
-        
-        
-               
       </div>
     </>
   );
