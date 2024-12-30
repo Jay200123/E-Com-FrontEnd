ff3806df1472bc45cf1d48242c399e3a76217a88
Jay200123 <kenken.ayonon@gmail.com>
2024-12-30 19:40:15 +0800

    Export CustomerLayout component from layouts index

--- src/layouts/index.ts
+++ src/layouts/index.ts
@@ -1,7 +1,9 @@
 import MainLayout from "./MainLayout";
 import ProductLayout from "./ProductLayout";
+import CustomerLayout from "./CustomerLayout";
 
 export { 
     MainLayout,
     ProductLayout,
+    CustomerLayout
 };
