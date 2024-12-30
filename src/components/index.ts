5661b250dc405c2ba956717475668e18f237612c
Jay200123 <kenken.ayonon@gmail.com>
2024-12-30 19:39:24 +0800

    Add CustomerNavbar and ProtectedRoute to components export

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,10 +2,14 @@ import Navbar from "./Navbar/Navbar";
 import Footer from "./Footer/Footer";
 import Carousel from "./Carousel/Carousel";
 import ProductSidebar from "./Sidebar/ProductSidebar";
+import ProtectedRoute from "./ProtectedRoutes";
+import CustomerNavbar from "./Navbar/CustomerNavbar";
 
 export {
     Navbar, 
     Footer,
     Carousel,
-    ProductSidebar  
+    ProductSidebar,
+    CustomerNavbar,
+    ProtectedRoute  
 }
