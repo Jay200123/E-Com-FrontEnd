253ea874a06268fad9046a9a72d919b398c950c1
Jay200123 <kenken.ayonon@gmail.com>
2024-12-30 20:01:46 +0800

    Add user store to state exports

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,4 +1,9 @@
 import { useAuthenticationStore } from "./auth";
 import { useProductStore } from "./api/product";
+import { useUserStore } from "./api/user";
 
-export { useAuthenticationStore, useProductStore };
+export { 
+    useAuthenticationStore,
+    useProductStore, 
+    useUserStore
+};
