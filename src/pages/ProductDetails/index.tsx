fe48249e91a06fe49eceb54b1de55caf1e2f9f33
Jay200123 <kenken.ayonon@gmail.com>
2024-12-30 19:40:35 +0800

    Fetch product details dynamically and update UI with product information

--- src/pages/ProductDetails/index.tsx
+++ src/pages/ProductDetails/index.tsx
@@ -1,34 +1,53 @@
-import ImageOne from "../../assets/mobile-3jpeg.jpg";
+import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import { useProductStore } from "../../state/store";
 
 export default function () {
-    const back = ()=>{
-        window.history.back();
-    }
+  const { id } = useParams<{ id: string }>();
+
+  const { getProductById } = useProductStore();
+
+  const { data } = useQuery({
+    queryKey: ["product", id],
+    queryFn: () => getProductById(id!),
+    enabled: !!id,
+  });
+
+  const back = () => {
+    window.history.back();
+  };
+
   return (
     <>
       <div className="flex flex-col items-center w-full h-screen md:flex-row">
         <div className="relative w-1/2 h-full ">
-            <i onClick={back} className="absolute m-2 text-2xl cursor-pointer fa fa-arrow-left"></i>
+          <i
+            onClick={back}
+            className="absolute m-2 text-2xl cursor-pointer fa fa-arrow-left"
+          ></i>
 
-          <img src={ImageOne} className="object-contain w-full h-full" />
+          {data?.image && data?.image?.length > 1 ? (
+            <img
+              className="object-contain w-full h-full"
+              src={
+                data?.image[Math.floor(Math.random() * data?.image.length)]?.url
+              }
+              alt="test image"
+            />
+          ) : (
+            <img
+              className="object-contain w-full h-full"
+              src={data?.image[0]?.url || ""}
+              alt="image"
+            />
+          )}
         </div>
         <div className="flex flex-col w-1/2 h-full p-2 justify-evenly">
-          <h3 className="text-2xl text-center">Samsung A05s</h3>
+          <h3 className="text-2xl text-center">{data?.product_name} ({data?.color})</h3>
           <h3 className="text-lg">Product Description:</h3>
-          <p className="text-sm">
-            The Samsung Galaxy A05s is a "budget-friendly" smartphone that
-            features a large 6.7-inch FHD+ display with a 90Hz screen refresh
-            rate, 50MP + 2MP + 2MP triple rear cameras with LED flash, and a
-            single 13MP selfie camera. It runs on a Qualcomm Snapdragon 680
-            processor with 4GB of RAM, Android 13 OS, and the One UI interface.
-            There's also a side-mounted fingerprint scanner, 4G LTE
-            connectivity, dual-band Wi-Fi, Dolby ATMOS audio, and 128GB of
-            expandable storage up to 1TB. A built-in 5,000mAh battery powers the
-            device with support for 25W fast charging technology. It comes in
-            two color choices - black and light green.
-          </p>
+          <p className="text-sm">{data?.description}</p>
           <h3 className="text-lg">Unit Price:</h3>
-          <p className="text-sm">₱6,490.00</p>
+          <p className="text-sm">₱{data?.price}</p>
           <h3 className="text-lg">Colors Available:</h3>
           <div className="flex space-x-4">
             <div className="w-8 h-8 bg-black rounded-full"></div>
@@ -36,7 +55,7 @@ export default function () {
           </div>
 
           <h3 className="text-lg">Available Stocks:</h3>
-          <p className="text-sm">20 pcs.</p>
+          <p className="text-sm">{data?.quantity} pcs</p>
 
           <div className="flex flex-col items-center w-full p-4">
             <div className="flex items-center space-x-2">
