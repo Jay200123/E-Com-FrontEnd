2f7ff4fdd705b060bc81efa28be3060e4305ad8b
Jay200123 <kenken.ayonon@gmail.com>
2024-12-30 19:40:54 +0800

    Implement formik for SignIn component with validation and navigation on successful login

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,8 +1,45 @@
 import ImageOne from "../../assets/SignIn.avif";
+import { useFormik } from "formik";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+import { useAuthenticationStore } from "../../state/store";
+import { SignInValidationSchema } from "../../validations";
 
 export default function () {
+  const navigate = useNavigate();
+  const { login } = useAuthenticationStore();
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: SignInValidationSchema,
+    onSubmit: async (values) => {
+      try {
+        const res = await login(values?.email, values?.password);
+        if (res?.role === "User") {
+          toast.success("Sign In Successfully");
+          navigate("/user/profile");
+        } else {
+          toast.success("Welcome Admin");
+          navigate("/admin/dashboard");
+        }
+      } catch (err) {
+        toast.error("Invalid email or password", {
+          position: "top-center",
+          autoClose: 5000,
+          closeOnClick: true,
+        });
+      }
+    },
+  });
+
   return (
-    <form className="flex items-center w-full h-[32rem] rounded-md shadow-md">
+    <form
+      onSubmit={formik?.handleSubmit}
+      className="flex items-center w-full h-[32rem] rounded-md shadow-md"
+    >
       <div className="w-1/2 h-full">
         <img
           src={ImageOne}
@@ -19,14 +56,31 @@ export default function () {
 
         <input
           type="email"
+          name="email"
+          id="email"
+          onBlur={formik?.handleBlur}
+          onChange={formik?.handleChange}
+          value={formik?.values?.email}
           placeholder="Email"
           className="p-1 mb-4 text-[1rem] border-b border-gray-700 rounded-sm"
         />
+        {formik.touched.email && formik.errors.email ? (
+          <div className="text-sm text-red-500">{formik.errors.email}</div>
+        ) : null}
+
         <input
           type="password"
+          name="password"
+          id="password"
+          onBlur={formik?.handleBlur}
+          onChange={formik?.handleChange}
+          value={formik?.values?.password}
           placeholder="Password"
           className="p-1 mb-4 text-[1rem] border-b border-gray-700 rounded-sm"
         />
+        {formik.touched.password && formik.errors.password ? (
+          <div className="text-sm text-red-500">{formik.errors.password}</div>
+        ) : null}
         <div className="flex items-center mb-4 space-x-2">
           <input type="checkbox" className="p-1" />
           <span className="text-sm">
@@ -34,7 +88,10 @@ export default function () {
             <span className="font-bold">Terms of Use</span>
           </span>
         </div>
-        <button className="px-4 py-2 text-white bg-black rounded-md hover:bg-blue-600">
+        <button
+          type="submit"
+          className="px-4 py-2 text-white bg-black rounded-md hover:bg-blue-600"
+        >
           Sign In
         </button>
       </div>
