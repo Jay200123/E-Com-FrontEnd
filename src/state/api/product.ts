a387f53ca48760a8f37dcfa4ed8147f00d4d64a8
Jay200123 <kenken.ayonon@gmail.com>
2025-01-01 13:50:00 +0800

    Refactor product API calls to use centralized axios instance

--- src/state/api/product.ts
+++ src/state/api/product.ts
@@ -1,30 +1,30 @@
 import { create } from "zustand";
 import { ProductState } from "../../interface/product";
-import axios from "axios";
+import api from "../axios";
 
 export const useProductStore = create<ProductState>((set) => ({
   loading: false,
   message: "",
   getAllProducts: async () => {
-    const res = await axios.get("http://localhost:4000/api/v1/products");
+    const res = await api.get(`/products`);
     set({ message: "Product records found", loading: false });
     return res.data.details;
   },
   getProductById: async (id: string) => {
-    const res = await axios.get(`http://localhost:4000/api/v1/product/${id}`);
+    const res = await api.get(`/product/${id}`);
     set({ message: "Product record found", loading: false });
     return res.data.details;
   },
   addProduct: async (data: FormData) => {
-    await axios.post("http://localhost:4000/api/v1/products", data);
+    await api.post("/products", data);
     set({ message: "Product record added", loading: false });
   },
   updateProduct: async (id: string, data: FormData) => {
-    await axios.patch(`http://localhost:4000/api/v1/products/${id}`, data);
+    await api.patch(`/products/${id}`, data);
     set({ message: "Product record updated", loading: false });
   },
   deleteProduct: async (id: string) => {
-    await axios.delete(`http://localhost:4000/api/v1/products/${id}`);
+    await api.delete(`/products/${id}`);
     set({ message: "Product record deleted", loading: false });
   },
 }));
