9ddc51cd89dfba41b7f50f47001517e3deda779a
Jay200123 <kenken.ayonon@gmail.com>
2025-01-01 13:50:06 +0800

    Refactor user API calls to use centralized axios instance

--- src/state/api/user.ts
+++ src/state/api/user.ts
@@ -1,35 +1,30 @@
 import { create } from "zustand";
 import { UserState } from "../../interface";
-import axios from "axios";
+import api from "../axios";
 
 export const useUserStore = create<UserState>((set) => ({
   message: "",
   getAllUsers: async () => {
-    const res = await axios.get("http://localhost:4000/api/v1/users");
+    const res = await api.get("/users");
     return res?.data?.details;
 
     set({ message: res?.data?.message });
   },
   getUserById: async (id: string) => {
-    const res = await axios.get(`http://localhost:4000/api/v1/user/${id}`);
+    const res = await api.get(`/user/${id}`);
     return res?.data?.details;
     set({ message: res?.data?.message });
   },
   addUser: async (formData) => {
-    const res = await axios.post("http://localhost:4000/api/v1/user", formData);
+    const res = await api.post("/users", formData);
     set({ message: res?.data?.message });
   },
   updateUserById: async (id: string, formData) => {
-    const res = await axios.patch(
-      `http://localhost:4000/api/v1/user/edit/${id}`,
-      formData
-    );
+    const res = await api.patch(`/user/edit/${id}`, formData);
     set({ message: res?.data?.message });
   },
   deleteUserById: async (id) => {
-    const res = await axios.delete(
-      `http://localhost:4000/api/v1/user/delete/${id}`
-    );
+    const res = await api.delete(`/user/${id}`);
     set({ message: res?.data?.message });
   },
 }));
