699be46fe11c5a2a999d8635a8ba26397f5499cc
Jay200123 <kenken.ayonon@gmail.com>
2025-01-01 20:57:09 +0800

    Enhance checkout process with payment method selection and user details integration

--- src/pages/Cart/checkout.tsx
+++ src/pages/Cart/checkout.tsx
@@ -1,23 +1,65 @@
-import ImageTwo from "../../assets/mobile-3jpeg.jpg";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { 
+  useAuthenticationStore,
+  useCartStore
+} from "../../state/store";
+import { useFormik } from "formik";
+import { toast } from "react-toastify";
 
 export default function () {
     const navigate = useNavigate();
-  const [paymentMethod, setPaymentMethod] = useState<"card" | "delivery">(
-    "delivery"
+  const [paymentMethod, setPaymentMethod] = useState<"cash" | "credit card">(
+    "credit card"
   );
 
-  const togglePaymentMethod = (method: "card" | "delivery") => {
+  const { cart: CartItems, clearCart } = useCartStore();
+  const { user } = useAuthenticationStore();
+
+  const togglePaymentMethod = (method: "cash" | "credit card") => {
     setPaymentMethod(method);
   };
 
-  const cart = ()=>{
-        navigate("/sample/cart")
+  const totalAmount = CartItems.reduce((acc, product) => {
+    return acc + product.price * product.orderQuantity;
+  }, 0);
+
+  const totalOrders = CartItems.reduce((acc, product) => {
+    return acc + product.orderQuantity;
+  }, 0);
+
+  let shipping = 50.0;
+
+  if (totalAmount > 5000) {
+    shipping = 0;
   }
+
+  const formik = useFormik({
+    initialValues: {
+      user: user?._id,
+      products: CartItems?.map((p) => ({
+        product: p?._id,
+        quantity: p?.orderQuantity,
+      })),
+      payment: paymentMethod,   
+
+    },
+    onSubmit: (values) => {
+      console.log(values)
+      navigate("/users");
+      clearCart();
+      toast.success("Order Placed Successfully"); 
+    }
+
+  })
+
+  const cart = ()=>{
+    navigate("/user/cart")
+  }
+
   return (
     <>
-      <div className="flex flex-col items-center w-full h-screen md:flex-row ">
+      <form onSubmit={formik.handleSubmit} className="flex flex-col items-center w-full h-screen md:flex-row ">
         <div className="w-[70%] flex flex-col h-full border border-r-4 border-gray-500 ">
           <div className="w-full h-[30%] flex flex-col justify-center items-center">
             <h3 className="text-sm font-normal md:text-2xl md:font-bold">
@@ -29,9 +71,9 @@ export default function () {
             </h3>
             <div className="flex flex-col items-center justify-start w-full px-3 md:flex-row md:justify-between">
               <button
-                onClick={() => togglePaymentMethod("delivery")}
+                onClick={() => togglePaymentMethod("credit card")}
                 className={`w-[10rem] p-2 text-sm rounded-md border ${
-                  paymentMethod === "delivery"
+                  paymentMethod === "credit card"
                     ? "bg-blue-500 text-white border-blue-500"
                     : "border-black"
                 }`}
@@ -40,9 +82,9 @@ export default function () {
               </button>
 
               <button
-                onClick={() => togglePaymentMethod("card")}
+                onClick={() => togglePaymentMethod("cash")}
                 className={`w-[10rem] p-2 text-sm rounded-md border ml-4 ${
-                  paymentMethod === "card"
+                  paymentMethod === "cash"
                     ? "bg-blue-500 text-white border-blue-500"
                     : "border-black"
                 }`}
@@ -56,7 +98,7 @@ export default function () {
             </div>
           </div>
 
-          {paymentMethod === "card" ? (
+          {paymentMethod === "cash" ? (
             <div className="flex flex-col w-full p-2 h-[70%]">
               <h3 className="text-lg font-medium">Credit Card Information</h3>
               <div className="flex flex-col w-1/2 p-2">
@@ -66,6 +108,9 @@ export default function () {
                 <input
                   type="text"
                   placeholder="John Doe"
+                  onBlur={formik.handleBlur}
+                  onChange={formik.handleChange}
+                  value={user?.fullname}
                   className="p-2 mb-4 text-[1rem] border border-gray-300 rounded-md"
                 />
               </div>
@@ -112,8 +157,9 @@ export default function () {
                   </label>
                   <input
                     type="text"
-                    placeholder="John Doe"
-                    className="p-1 mb-4 text-[1rem] border border-gray-300 rounded-md"
+                    readOnly  
+                    placeholder={user?.fullname}
+                    className="placeholder:text-black p-1 mb-4 text-[1rem] border border-gray-300 rounded-md"
                   />
                 </div>
                 <div className="flex flex-col w-1/2 p-2">
@@ -122,8 +168,9 @@ export default function () {
                   </label>
                   <input
                     type="text"
-                    placeholder="+639 994 339 4563"
-                    className="p-1 mb-4 text-[1rem] border border-gray-300 rounded-md"
+                    readOnly
+                    placeholder={user?.contact_number}
+                    className="placeholder:text-black p-1 mb-4 text-[1rem] border border-gray-300 rounded-md"
                   />
                 </div>
               </div>
@@ -134,8 +181,9 @@ export default function () {
                   </label>
                   <input
                     type="text"
-                    placeholder="123 Main St. New York, NY"
-                    className="p-1 mb-4 text-[1rem] border border-gray-300 rounded-md"
+                    readOnly
+                    placeholder={user?.address}
+                    className="placeholder:text-black p-1 mb-4 text-[1rem] border border-gray-300 rounded-md"
                   />
                 </div>
                 <div className="flex flex-col w-1/2 p-2">
@@ -144,8 +192,9 @@ export default function () {
                   </label>
                   <input
                     type="text"
-                    placeholder="New York"
-                    className="p-1 mb-4 text-[1rem] border border-gray-300 rounded-md"
+                    readOnly
+                    value={user?.city}
+                    className="placeholder:text-black p-1 mb-4 text-[1rem] border border-gray-300 rounded-md"
                   />
                 </div>
               </div>
@@ -156,8 +205,9 @@ export default function () {
                   </label>
                   <input
                     type="text"
-                    placeholder="johndoe@gmail.com"
-                    className="p-1 mb-4 text-[1rem] border border-gray-300 rounded-md"
+                    readOnly
+                    placeholder={user?.email} 
+                    className="placeholder:text-black p-1 mb-4 text-[1rem] border border-gray-300 rounded-md"
                   />
                 </div>
               </div>
@@ -171,20 +221,39 @@ export default function () {
           </h3>
           <div className="flex flex-col justify-between h-[30rem] ">
             <div className="flex flex-col w-full justify-start max-h-[300px] overflow-y-auto border p-1 border-white">
-              <div className="flex flex-row items-center w-full mb-3 h-[120px] border border-gray-500 rounded-md">
-                <div className="w-[30%]">
-                  <img src={ImageTwo} className="object-contain w-40 h-40" />
-                </div>
-                <div className="w-[70%] flex flex-col justify-between h-full">
-                  <h3 className="text-[1rem]">Samsung A05s</h3>
-                  <h3 className="text-[1rem]">
-                    x <span className="mr-1">1</span>
-                  </h3>
-                  <h3 className="text-[1rem]">
-                    Item Subtotal: <span className="mr-1">6,490</span>
-                  </h3>
-                </div>
-              </div>
+              {CartItems.map((p) => (
+                 <div key={p?._id} className="flex flex-row items-center w-full mb-3 h-[120px] border border-gray-500 rounded-md">
+                 <div className="w-[30%]">
+                 {p?.image?.length > 1 ? (
+                    <img
+                      className="object-contain w-40 h-40"  
+                      src={
+                        p?.image[
+                          Math.floor(Math.random() * p?.image.length)
+                        ]?.url
+                      }
+                      alt="test image"
+                    />
+                  ) : (
+                    <img
+                    className="object-contain w-40 h-40"
+                    src={p?.image[0]?.url || ""}
+                      alt="image"
+                    />
+                  )}
+                 </div>
+                 <div className="w-[70%] flex flex-col justify-between h-full">
+                   <h3 className="text-[1rem]">{p?.product_name}</h3>
+                   <h3 className="text-[1rem]">
+                     x <span className="mr-1">{p?.orderQuantity}</span>
+                   </h3>
+                   <h3 className="text-[1rem]">
+                     Item Subtotal: <span className="mr-1">{p?.orderQuantity * p?.price}</span>
+                   </h3>
+                 </div>
+               </div>
+              ))}
+             
             </div>
 
             <div className="w-full">
@@ -193,27 +262,27 @@ export default function () {
                   Order Subtotal:
                 </p>
                 <p className="text-[1rem] text-gray-700 font-semibold">
-                  6,490.00
+                  {totalAmount}
                 </p>
               </div>
               <div className="flex items-center justify-between p-2">
                 <p className="text-[1rem] text-gray-700 font-semibold">
                   Item Quantity:
                 </p>
-                <p className="text-[1rem] text-gray-700 font-semibold">x1</p>
+                <p className="text-[1rem] text-gray-700 font-semibold">{totalOrders}</p>
               </div>
               <div className="flex items-center justify-between p-2">
                 <p className="text-[1rem] text-gray-700 font-semibold">
                   Shipping:
                 </p>
-                <p className="text-[1rem] text-gray-700 font-semibold">0.00</p>
+                <p className="text-[1rem] text-gray-700 font-semibold">{shipping}</p>
               </div>
               <div className="flex items-center justify-between p-2">
                 <p className="text-[1rem] text-gray-700 font-semibold">
                   Total:
                 </p>
                 <p className="text-[1rem] text-gray-700 font-semibold">
-                  6,490.00
+                  {totalAmount + shipping}
                 </p>
               </div>
               <div className="flex items-center justify-center p-2">
@@ -224,7 +293,7 @@ export default function () {
             </div>
           </div>
         </div>
-      </div>
+      </form>
     </>
   );
 }
