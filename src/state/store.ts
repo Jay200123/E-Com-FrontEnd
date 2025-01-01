e5c912c6320a85ab8f17eb1c6c0b6524f996ee11
Jay200123 <kenken.ayonon@gmail.com>
2025-01-01 13:49:53 +0800

    Add useCartStore to state management exports

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,9 +1,11 @@
 import { useAuthenticationStore } from "./auth";
 import { useProductStore } from "./api/product";
 import { useUserStore } from "./api/user";
+import { useCartStore } from "./cart";
 
 export { 
     useAuthenticationStore,
     useProductStore, 
-    useUserStore
+    useUserStore,
+    useCartStore,
 };
