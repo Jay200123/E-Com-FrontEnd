8e9c117c8b5c3e65b18c426faee2fefe21338e90
Jay200123 <kenken.ayonon@gmail.com>
2025-01-02 23:39:23 +0800

    Add Shop page component with basic layout and styling

---
+++ src/pages/Shop/index.tsx
@@ -0,0 +1,7 @@
+export default function () {
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-screen bg-black">
+      <h1 className="text-4xl font-bold">Shop Page</h1>
+    </div>
+  );
+}
