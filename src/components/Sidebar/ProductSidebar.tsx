276eea605465585b6039e68098d3630d8e86e408
Jay200123 <kenken.ayonon@gmail.com>
2025-01-02 23:38:08 +0800

    Add cart navigation and item count display in ProductSidebar

--- src/components/Sidebar/ProductSidebar.tsx
+++ src/components/Sidebar/ProductSidebar.tsx
@@ -1,15 +1,21 @@
+import { useCartStore } from "../../state/store";
+import { useNavigate } from "react-router-dom";
+
 export default function () {
+  const navigate = useNavigate();
+  const { cart } = useCartStore();  
+
   const back = ()=>{
     window.history.back();
   }
   return (
-    <div className="w-[10rem] h-screen bg-gray-700 text-white hidden md:flex flex-col justify-between items-center">
+    <div className="w-[10rem] h-screen bg-slate-50 border border-l-gray-500 text-black hidden md:flex flex-col justify-between items-center">
       <div className="flex flex-col justify-center p-1">
         <h3 className="text-3xl font-bold text-center">IT Shop</h3>
         <div className="relative flex justify-center">
-          <i className="text-3xl text-center cursor-pointer fa-solid fa-cart-shopping"></i>
+          <i onClick={()=>navigate(`/cart`)} className="text-3xl text-center cursor-pointer fa-solid fa-cart-shopping"></i>
           <p className="absolute top-0 right-0 flex items-center justify-center w-5 h-5 text-sm text-white bg-red-700 rounded-full">
-            0
+            {cart.length}
           </p>
         </div>
       </div>
