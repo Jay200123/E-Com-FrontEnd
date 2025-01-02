beffe21ea99dffec8dd72b5f56ca9299c8d3d5db
Jay200123 <kenken.ayonon@gmail.com>
2025-01-02 23:38:36 +0800

    Fix layout width in ProductLayout component

--- src/layouts/ProductLayout.tsx
+++ src/layouts/ProductLayout.tsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 export default function () {
   return (
     <>
-      <main className="flex items-center w-screen">
+      <main className="flex items-center w-full">
         <div className="w-[12.5%] ">
           <ProductSidebar />
         </div>
