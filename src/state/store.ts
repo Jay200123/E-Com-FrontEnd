8409ac162c346cf95caf6fea30abc27c44c34508
Jay200123 <kenken.ayonon@gmail.com>
2025-01-02 23:39:29 +0800

    Add order store integration to state management

--- src/state/store.ts
+++ src/state/store.ts
@@ -2,10 +2,12 @@ import { useAuthenticationStore } from "./auth";
 import { useProductStore } from "./api/product";
 import { useUserStore } from "./api/user";
 import { useCartStore } from "./cart";
+import { useOrderStore } from "./api/order";
 
 export { 
     useAuthenticationStore,
     useProductStore, 
     useUserStore,
     useCartStore,
+    useOrderStore,
 };
