6114f1f82d810a47144cefb6d207e194c2863e13
Jay200123 <kenken.ayonon@gmail.com>
2025-01-02 23:39:12 +0800

    Refactor MobileProducts component to integrate cart functionality and improve product filtering

--- src/pages/Products/MobileProducts.tsx
+++ src/pages/Products/MobileProducts.tsx
@@ -1,35 +1,41 @@
-import { useProductStore } from "../../state/store";
+import { useProductStore, useCartStore } from "../../state/store";
 import { useQuery } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
 
 export default function () {
+  const navigate = useNavigate();
   const { getAllProducts } = useProductStore();
+  const { addProduct } = useCartStore();
+
 
   const { data } = useQuery({
     queryKey: ["products"],
     queryFn: getAllProducts,
   });
 
-  // const filteredData = data?.filter((product) => product.category === "mobile");
+  const filteredData = data?.filter((product) => product.category.includes("Mobile"));
   return (
     <>
-      <div className="relative flex flex-col items-center w-full h-screen p-10 space-x-4 overflow-x-auto md:grid md:grid-cols-4 md:gap-4">
+      <div className="relative flex flex-col items-center w-full h-screen p-10 overflow-x-auto md:grid md:grid-cols-4 md:gap-4">
         <h3 className="absolute text-2xl font-bold left-1 top-1">
           Mobile Phones
         </h3>
-        {data?.map((p) => (
+        {filteredData?.map((p) => (
           <div
             key={p?._id}
+            onClick={() => navigate(`/product/${p?._id}`)}  
             className="min-w-[15rem] max-w-[18rem] cursor-pointer transition-all duration-500 h-auto p-2 flex-shrink-0 border border-gray-300 rounded-md"
           >
             {p?.image?.length > 1 ? (
               <img
-                className="object-cover w-full h-64"
+                className="object-cover w-full h-32 md:h-64"
+                onClick={() => navigate(`/product/${p?._id}`)}  
                 src={p?.image[Math.floor(Math.random() * p?.image.length)]?.url}
                 alt="test image"
               />
             ) : (
               <img
-                className="object-cover w-full h-64"
+                className="object-cover w-full h-32 md:h-64"
                 src={p?.image[0]?.url || ""}
                 alt="image"
               />
@@ -39,7 +45,8 @@ export default function () {
             </p>
             <div className="flex items-center justify-between">
               <p className="font-semibold">₱{p?.price}</p>
-              <i className="text-2xl transition-all duration-500 cursor-pointer fa-solid hover:text-red-500 fa-cart-shopping"></i>
+              <i onClick={()=>addProduct(p, 1)} className="text-2xl transition-all duration-500 cursor-pointer fa-solid hover:text-red-500 fa-cart-shopping">
+              </i>
             </div>
           </div>
         ))}
