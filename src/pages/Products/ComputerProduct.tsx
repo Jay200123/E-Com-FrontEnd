b62dbdf4f365f617468d25a4abb98dd818ea5b3c
Jay200123 <kenken.ayonon@gmail.com>
2025-01-03 21:18:04 +0800

    Enhance ComputerProduct: Integrate price filtering and add star rating display

--- src/pages/Products/ComputerProduct.tsx
+++ src/pages/Products/ComputerProduct.tsx
@@ -1,41 +1,46 @@
-import { useProductStore, useCartStore } from "../../state/store";
+import {
+  useProductStore,
+  useCartStore,
+  useFilterStore,
+} from "../../state/store";
 import { useQuery } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
+import { FaStar } from "react-icons/fa";
 
 export default function () {
   const navigate = useNavigate();
-  const { getAllProducts } = useProductStore();
+  const { products, getAllComputers } = useProductStore();
   const { addProduct } = useCartStore();
+  const { price } = useFilterStore();
 
-
-  const { data } = useQuery({
+  useQuery({
     queryKey: ["products"],
-    queryFn: getAllProducts,
+    queryFn: getAllComputers,
   });
 
-  const filteredData = data?.filter((product) => product.category.includes("Computer"));  
+  const filteredProducts = products?.filter((p) =>
+    p?.price.toString().includes(price.toString())
+  );
+
   return (
     <>
-      <div className="relative flex flex-col items-center w-full h-screen p-10 mb-2 space-x-4 overflow-x-auto md:grid md:grid-cols-4 md:gap-4">
-        <h3 className="absolute text-2xl font-bold left-1 top-1">
-          Computers
-        </h3>
-        {filteredData?.map((p) => (
+      <div className="flex flex-col flex-wrap items-center justify-between w-full h-screen overflow-x-auto md:grid md:grid-cols-4">
+        {filteredProducts?.map((p) => (
           <div
             key={p?._id}
-            className="min-w-[15rem] max-w-[9rem] md:max-w-[18rem] cursor-pointer transition-all duration-500 h-auto p-2 flex-shrink-0 border border-gray-300 rounded-md"
+            className="max-w-[15rem] md:w-[16rem] m-3 h-auto cursor-pointer transition-all duration-500 p-2 border border-gray-300 rounded-md"
           >
             {p?.image?.length > 1 ? (
               <img
                 className="object-cover w-full h-32 md:h-64"
-                onClick={() => navigate(`/product/${p?._id}`)}  
+                onClick={() => navigate(`/product/${p?._id}`)}
                 src={p?.image[Math.floor(Math.random() * p?.image.length)]?.url}
                 alt="test image"
               />
             ) : (
               <img
                 className="object-cover w-full h-32 md:h-64"
-                onClick={() => navigate(`/product/${p?._id}`)} 
+                onClick={() => navigate(`/product/${p?._id}`)}
                 src={p?.image[0]?.url || ""}
                 alt="image"
               />
@@ -43,10 +48,19 @@ export default function () {
             <p className="font-semibold">
               {p.product_name}({p?.color})
             </p>
+            <div className="flex items-center flex-start">
+              <FaStar className="text-yellow-500 " />
+              <FaStar className="text-yellow-500" />
+              <FaStar className="text-yellow-500" />
+              <FaStar className="text-yellow-500" />
+              <FaStar className="text-yellow-500" />
+            </div>
             <div className="flex items-center justify-between">
               <p className="font-semibold">₱{p?.price}</p>
-              <i onClick={()=>addProduct(p, 1)} className="text-2xl transition-all duration-500 cursor-pointer fa-solid hover:text-red-500 fa-cart-shopping">
-              </i>
+              <i
+                onClick={() => addProduct(p, 1)}
+                className="text-2xl transition-all duration-500 cursor-pointer fa-solid hover:text-red-500 fa-cart-shopping"
+              ></i>
             </div>
           </div>
         ))}
