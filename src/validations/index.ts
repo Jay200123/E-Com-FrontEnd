0024dc533c0353657d1d4b949c7e0d05b4fd7e78
Jay200123 <kenken.ayonon@gmail.com>
2025-01-04 12:00:08 +0800

    Add EditUserValidationSchema to validation exports

--- src/validations/index.ts
+++ src/validations/index.ts
@@ -1,7 +1,9 @@
 import SignInValidationSchema from "./signInValidationSchema";
 import SignUpValidationSchema from "./signUpValidationSchema";
+import EditUserValidationSchema from "./editUserValidation";
 
-export { 
-    SignInValidationSchema,
-     SignUpValidationSchema
- };
+export {
+  SignInValidationSchema,
+  SignUpValidationSchema,
+  EditUserValidationSchema,
+};
