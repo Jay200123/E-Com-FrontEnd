e3553e8eb44560681326071642afdb35c33a67a3
Jay200123 <kenken.ayonon@gmail.com>
2025-01-04 12:00:00 +0800

    Enhance Filter: Add name filtering to the filter state

--- src/state/filter/index.ts
+++ src/state/filter/index.ts
@@ -1,16 +1,19 @@
 import { create } from "zustand";
 
 interface FilterState {
+  name: string;
   price: number;
   brand: string;
-  setFilter: (price: number, brand: string) => void;
+  setFilter: (name: string, price: number, brand: string) => void;
 }
 
 export const useFilterStore = create<FilterState>((set) => ({
+  name: "",
   price: 0,
   brand: "",
-  setFilter: (price: number, brand: string) =>
+  setFilter: (name: string, price: number, brand: string) =>
     set({
+      name: name,
       price: price,
       brand: brand,
     }),
