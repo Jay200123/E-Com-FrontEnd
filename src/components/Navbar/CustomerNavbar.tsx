cc9be652ee96ff9ade66dea76b58e928dbdf81b2
Jay200123 <kenken.ayonon@gmail.com>
2025-01-04 17:09:22 +0800

    Refactor Navbar component for responsive text sizing

--- src/components/Navbar/CustomerNavbar.tsx
+++ src/components/Navbar/CustomerNavbar.tsx
@@ -36,33 +36,35 @@ export default function () {
   return (
     <nav className="w-full overflow-hidden h-[3.75rem] border border-gray-300 flex items-center justify-start md:justify-between p-1">
       <div className="items-center hidden md:block">
-        <i className="m-2 text-lg fa-solid fa-bag-shopping"></i>
-        <h3 className="text-lg font-bold"> IT Shop</h3>
+        <i className="m-2 text-xs md:text-sm fa-solid fa-bag-shopping"></i>
+        <h3 className="text-xs font-bold md:text-sm"> IT Shop</h3>
       </div>
       <div className="flex items-center justify-center">
         <ul className="flex flex-row items-start justify-start">
           <li
             onClick={home}
-            className="m-3 text-sm font-semibold cursor-pointer"
+            className="m-3 text-xs font-semibold cursor-pointer md:text-sm"
           >
             Home
           </li>
-          <li onClick={shop} className="m-3 text-sm font-semibold cursor-pointer">Shop</li>
-          <li className="m-3 text-sm font-semibold">About</li>
-          <li className="m-3 text-sm font-semibold">Contact Us</li>
+          <li onClick={shop}
+           className="m-3 text-xs font-semibold cursor-pointer md:text-sm"
+           >Shop</li>
+          <li className="m-3 text-xs font-semibold cursor-pointer md:text-sm">About</li>
+          <li className="m-3 text-xs font-semibold cursor-pointer md:text-sm">Contact Us</li>
         </ul>
       </div>
       <div className="flex items-center justify-between p-1">
         <i
           title="User Profile"
           onClick={profile}
-          className="m-2 text-lg cursor-pointer fa-regular fa-circle-user"
+          className="m-2 text-xs md:text-[1rem] cursor-pointer fa-regular fa-circle-user"
         ></i>
         <div className="relative p-1">
           <i
             title="Cart"
             onClick={cart}
-            className="m-2 text-lg cursor-pointer fa-solid fa-cart-shopping"
+            className="m-2 text-xs md:text-[1rem]  cursor-pointer fa-solid fa-cart-shopping"
           ></i>
           <p className="absolute top-0 right-0 w-5 h-5 p-[1px] text-sm text-center text-white bg-red-600 rounded-full">
             {CartItems?.length}
@@ -71,7 +73,7 @@ export default function () {
         <i
           title="Logout"
           onClick={handleLogout}
-          className="m-2 text-lg cursor-pointer fa-solid fa-right-to-bracket"
+          className="m-2 text-xs md:text-[1rem] cursor-pointer fa-solid fa-right-to-bracket"
         ></i>
       </div>
     </nav>
