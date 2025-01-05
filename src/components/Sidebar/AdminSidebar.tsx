df7ffd29adc1a38457f0771086613f47be22bcc8
Jay200123 <kenken.ayonon@gmail.com>
2025-01-05 14:44:52 +0800

    Add AdminSidebar component for navigation and logout functionality

---
+++ src/components/Sidebar/AdminSidebar.tsx
@@ -0,0 +1,73 @@
+import { useNavigate } from "react-router-dom";
+import { useAuthenticationStore } from "../../state/store";
+import { toast } from "react-toastify";
+
+export default function () {
+  const navigate = useNavigate();
+  const { logout } = useAuthenticationStore();
+
+  const users = () => {
+    navigate("/users/table");
+  };
+
+  const products = () => {
+    navigate("/products/table");
+  }
+
+  const dashboard = () => {
+    navigate("/admin/dashboard");
+  }
+
+  const handleLogout = async () => {
+    try {
+      await logout();
+      navigate("/signin");
+      toast.success("Successfully Log Out");
+    } catch (error) {
+      toast.error("Error Logging Out");
+    }
+  };
+
+  return (
+    <div className="flex-col items-center justify-between hidden w-full h-screen text-black border bg-slate-50 border-l-gray-500 md:flex">
+      <div className="flex flex-col justify-center p-1">
+        <h3 className="text-2xl font-bold text-gray-700">IT Shop</h3>
+      </div>
+      <div className="flex flex-col w-full space-y-4 p-[2px]">
+        <ul>
+          <li onClick={dashboard} className="text-gray-700 cursor-pointer text-[1rem] font-medium mb-4">
+            <i className="mr-3 fa-solid fa-chart-simple"></i>Overview
+          </li>
+          <li
+            onClick={users}
+            className="text-gray-700 cursor-pointer text-[1rem] font-medium mb-4"
+          >
+            <i className="mr-3 fa-solid fa-user"></i>Users
+          </li>
+
+          <li onClick={products} className="text-gray-700 cursor-pointer text-[1rem] font-medium mb-4">
+            <i className="mr-3 fa-solid fa-mobile"></i>Products
+          </li>
+          <li className="text-gray-700 text-[1rem] font-medium mb-4">
+            <i className="mr-3 fa-solid fa-cart-shopping"></i>Orders
+          </li>
+          <li className="text-gray-700 text-[1rem] font-medium mb-4">
+            <i className="mr-3 fa-solid fa-receipt"></i>Checkout
+          </li>
+          <li className="text-gray-700 text-[1rem] font-medium mb-4">
+            <i className="mr-3 fa-solid fa-gear"></i>Setting
+          </li>
+        </ul>
+      </div>
+
+      <div className="w-full mb-1">
+        <button
+          onClick={handleLogout}
+          className="px-4 py-2 text-[1rem font-semibold text-gray-700"
+        >
+          <i className="fa-solid fa-right-from-bracket"></i> Log Out
+        </button>
+      </div>
+    </div>
+  );
+}
