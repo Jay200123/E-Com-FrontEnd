6481ff6c75838f0916895f605431872b96bbacf5
Jay200123 <kenken.ayonon@gmail.com>
2025-01-05 14:44:43 +0800

    Add AdminSidebar and AdminNavbar to component exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,14 +2,18 @@ import Navbar from "./Navbar/Navbar";
 import Footer from "./Footer/Footer";
 import Carousel from "./Carousel/Carousel";
 import ProductSidebar from "./Sidebar/ProductSidebar";
+import AdminSideBar from "./Sidebar/AdminSidebar";
 import ProtectedRoute from "./ProtectedRoutes";
 import CustomerNavbar from "./Navbar/CustomerNavbar";
+import AdminNavbar from "./Navbar/AdminNavbar";
 
 export {
-    Navbar, 
-    Footer,
-    Carousel,
-    ProductSidebar,
-    CustomerNavbar,
-    ProtectedRoute  
-}
+  Navbar,
+  Footer,
+  Carousel,
+  ProductSidebar,
+  AdminSideBar,
+  CustomerNavbar,
+  AdminNavbar,
+  ProtectedRoute,
+};
