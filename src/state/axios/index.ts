7bc910ed8e83ed06443ec33eb918dedfe85f439d
Jay200123 <kenken.ayonon@gmail.com>
2025-01-05 14:45:39 +0800

    Update API base URL for local development

--- src/state/axios/index.ts
+++ src/state/axios/index.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://e-com-backend-peach.vercel.app/api/v1",
+  // baseURL: "https://e-com-backend-peach.vercel.app/api/v1",
+  baseURL: "http://localhost:4000/api/v1",
 });
 
 api.interceptors.request.use((config) => {
