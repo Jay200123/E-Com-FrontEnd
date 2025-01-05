a4ac79659b343fb0443ec288ffa6a44808258272
Jay200123 <kenken.ayonon@gmail.com>
2025-01-05 14:45:01 +0800

    Add BrandState interface for brand management operations

--- src/interface/brand.ts
+++ src/interface/brand.ts
@@ -1,6 +1,15 @@
 interface Brand {
-    _id: string;
-    brand_name: string; 
+  _id: string;
+  brand_name: string;
 }
 
-export type { Brand };
+interface BrandState {
+  message: string;
+  getAllBrands: () => Promise<Brand[]>;
+  getBrandById: (id: string) => Promise<Brand>;
+  addBrand: (formData: FormData) => Promise<void>;
+  updateBrandById: (id: string, brand_name: string) => Promise<void>;
+  deleteBrandById: (id: string) => Promise<void>;
+}
+
+export type { BrandState, Brand };
