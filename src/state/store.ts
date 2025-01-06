924cc44c6d1f267f2440ec068f1af8a9092fb38f
Jay200123 <kenken.ayonon@gmail.com>
2025-01-06 19:22:23 +0800

    Add useBrandStore to state exports for brand management

--- src/state/store.ts
+++ src/state/store.ts
@@ -4,12 +4,14 @@ import { useUserStore } from "./api/user";
 import { useCartStore } from "./cart";
 import { useOrderStore } from "./api/order";
 import { useFilterStore } from "./filter";
+import { useBrandStore } from "./api/brand";
 
-export { 
-    useAuthenticationStore,
-    useProductStore, 
-    useUserStore,
-    useCartStore,
-    useOrderStore,
-    useFilterStore
+export {
+  useAuthenticationStore,
+  useProductStore,
+  useUserStore,
+  useCartStore,
+  useOrderStore,
+  useFilterStore,
+  useBrandStore,
 };
