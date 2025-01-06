35c24299c28683801d3b00364c379700a4534f95
Jay200123 <kenken.ayonon@gmail.com>
2025-01-06 19:22:37 +0800

    Add CreateProductValidationSchema to validation exports

--- src/validations/index.ts
+++ src/validations/index.ts
@@ -1,9 +1,11 @@
 import SignInValidationSchema from "./signInValidationSchema";
 import SignUpValidationSchema from "./signUpValidationSchema";
 import EditUserValidationSchema from "./editUserValidation";
+import CreateProductValidationSchema from "./createProductValidationSchema";
 
 export {
   SignInValidationSchema,
   SignUpValidationSchema,
   EditUserValidationSchema,
+  CreateProductValidationSchema,
 };
