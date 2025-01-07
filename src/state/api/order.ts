b62017690d05efa4fc32dcf0be27bc7a69cbc143
Jay200123 <kenken.ayonon@gmail.com>
2025-01-07 13:01:16 +0800

    Add order status methods for packed, shipped, and delivered states

--- src/state/api/order.ts
+++ src/state/api/order.ts
@@ -2,19 +2,18 @@ import { OrderState } from "../../interface";
 import { create } from "zustand";
 import api from "../axios";
 
+
 export const useOrderStore = create<OrderState>((set) => ({
   message: "",
   getAllOrders: async () => {
     const res = await api.get("/orders");
-    return res.data.details;
-
     set({ message: res.data.message });
+    return res.data.details;
   },
-  getOrderById: async (id: string) => {
+  getOrderById: async (id) => {
     const res = await api.get(`/order/${id}`);
-    return res.data.details;
-
     set({ message: res.data.message });
+    return res.data.details;
   },
   addOrder: async (values) => {
     const res = await api.post("/orders", values);
@@ -24,6 +23,19 @@ export const useOrderStore = create<OrderState>((set) => ({
     const res = await api.put(`/order/edit/${id}`, formData);
     set({ message: res.data.message });
   },
+  packedOrder: async (id) => {
+    const res = await api.patch(`/order/packed/${id}`);
+    set({ message: res.data.message });
+  },
+  shippedOrder: async (id) => {
+    const res = await api.patch(`/order/shipped/${id}`);
+    set({ message: res.data.message });
+  },
+  deliveredOrder: async (id) => {
+    const res = await api.patch(`/order/delivered/${id}`);
+    set({ message: res.data.message });
+  },
+
   deleteOrderById: async (id) => {
     const res = await api.delete(`/order/delete/${id}`);
     set({ message: res.data.message });
