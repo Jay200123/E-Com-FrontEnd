bcf6c140a8b4938f8e5a07d6ba77244d17702596
Jay200123 <kenken.ayonon@gmail.com>
2025-01-07 21:29:46 +0800

    Add cursor pointer style to Orders list item in AdminSidebar

--- src/components/Sidebar/AdminSidebar.tsx
+++ src/components/Sidebar/AdminSidebar.tsx
@@ -52,7 +52,7 @@ export default function () {
           <li onClick={products} className="text-gray-700 cursor-pointer text-[1rem] font-medium mb-4">
             <i className="mr-3 fa-solid fa-mobile"></i>Products
           </li>
-          <li onClick={orders} className="text-gray-700 text-[1rem] font-medium mb-4">
+          <li onClick={orders} className="text-gray-700 cursor-pointer text-[1rem] font-medium mb-4">
             <i className="mr-3 fa-solid fa-cart-shopping"></i>Orders
           </li>
           <li className="text-gray-700 text-[1rem] font-medium mb-4">
