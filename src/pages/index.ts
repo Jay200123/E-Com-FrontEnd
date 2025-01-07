fccfd885e0f1732188f6b8fc18e023741b66b223
Jay200123 <kenken.ayonon@gmail.com>
2025-01-07 12:32:39 +0800

    Add OrderTable import to index for improved module accessibility

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -7,17 +7,18 @@ import LaptopProducts from "./Products/LaptopProducts";
 import ComputerProducts from "./Products/ComputerProduct";
 import Profile from "./Profile";
 import ProductDetails from "./ProductDetails";
-import Checkout from "./Cart/checkout";  
+import Checkout from "./Cart/checkout";
 import Cart from "./Cart";
 import Shop from "./Shop";
 import EditProfile from "./EditProfile";
 import OrderDetails from "./OrderDetails";
-import Dashboard from "./Dasboard"; 
+import Dashboard from "./Dasboard";
 import UserTable from "./UserTable";
-import GetUserById from "./UserTable/getUserById"; 
+import GetUserById from "./UserTable/getUserById";
 import ProductTable from "./ProductTable";
 import CreateProduct from "./ProductTable/createProduct";
 import EditProduct from "./ProductTable/editProduct";
+import OrderTable from "./OrderTable";
 
 export {
   Home,
@@ -40,4 +41,5 @@ export {
   ProductTable,
   CreateProduct,
   EditProduct,
+  OrderTable,
 };
