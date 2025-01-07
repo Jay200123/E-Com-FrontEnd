2f1ac15ee4a19936dbaebdb09aa04db4c007ab4d
Jay200123 <kenken.ayonon@gmail.com>
2025-01-07 12:32:34 +0800

    Improve AdminLayout styling for better responsiveness and overflow handling

--- src/layouts/AdminLayout.tsx
+++ src/layouts/AdminLayout.tsx
@@ -4,11 +4,11 @@ import { Outlet } from "react-router-dom";
 export default function () {
   return (
     <>
-      <main className="flex items-center w-full">
-        <div className="hidden md:block w-[15%] border ">
+      <main className="flex items-center w-full md:h-full">
+        <div className="hidden md:block h-full w-[15%]">
           <AdminSideBar />
         </div>
-        <div className="w-full md:w-[85.5%] h-screen border">
+        <div className="w-[85.5%] h-screen overflow-y-auto">
           <AdminNavbar />
           <Outlet />
         </div>
