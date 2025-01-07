5b50b0b28c0a240d9c7e11e7ebe202fedbee61f6
Jay200123 <kenken.ayonon@gmail.com>
2025-01-07 21:54:48 +0800

    Add terms agreement checkbox to SignUp component for user consent

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -4,10 +4,12 @@ import { useFormik } from "formik";
 import { SignUpValidationSchema } from "../../validations";
 import { useUserStore } from "../../state/store";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export default function () {
   const navigate = useNavigate();
   const { addUser } = useUserStore();
+  const [term, setTerm] = useState(false);
 
   const formik = useFormik({
     initialValues: {
@@ -43,15 +45,18 @@ export default function () {
   });
   return (
     <form
-      onSubmit={formik.handleSubmit}
-      className="flex items-center w-full h-[36rem] rounded-sm shadow-md"
+      onSubmit={(e) => {
+        e.preventDefault();
+        if (!term) {
+          toast.error("Please agree to the  Terms and conditions.");
+          return;
+        }
+        formik.handleSubmit(e);
+      }}
+      className="flex items-center w-full h-full overflow-hidden rounded-sm shadow-md"
     >
       <div className="hidden w-1/2 h-full md:block">
-        <img
-          src={ImageOne}
-          alt="Image"
-          className="object-cover w-full h-full"
-        />
+        <img src={ImageOne} alt="Image" className="object-cover h-[38rem]" />
       </div>
       <div className="flex flex-col w-full h-full p-4 md:w-1/2">
         <h3 className="mb-2 text-3xl font-semibold">Sign Up</h3>
@@ -89,7 +94,7 @@ export default function () {
           placeholder="Address"
           className="p-1 mb-4 text-[1rem] border-b border-gray-700 rounded-sm"
         />
-         <input
+        <input
           type="text"
           name="city"
           id="city"
@@ -130,13 +135,19 @@ export default function () {
           }}
         />
         <div className="flex items-center mb-4 space-x-2">
-          <input type="checkbox" className="p-2" />
+          <input
+            type="checkbox"
+            onChange={() => setTerm(!term)}
+            className="p-2"
+          />
           <span className="text-sm">
             I Agree with <span className="font-bold">Privacy Policy</span> and{" "}
             <span className="font-bold">Terms of Use</span>
           </span>
         </div>
-        <button className="px-4 py-2 text-white bg-black rounded-sm hover:bg-blue-600">
+        <button
+          className="px-4 py-2 text-white bg-black rounded-sm hover:bg-blue-600"
+        >
           Sign Up
         </button>
       </div>
