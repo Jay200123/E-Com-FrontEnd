399e8c2e434f1b129ad31cbbfe43ca5ab0ab3fbc
Jay200123 <kenken.ayonon@gmail.com>
2025-01-09 17:21:18 +0800

    Refactor Dashboard component to include CardInfo, OrderInfo, ProductInfo, and BrandInfo components for enhanced layout and functionality

--- src/pages/Dasboard/index.tsx
+++ src/pages/Dasboard/index.tsx
@@ -1,7 +1,17 @@
-export default function(){
-    return (
-        <>
-        this is dashboard page for admins!
-        </>
-    )
+import CardInfo  from "../Dasboard/CardInfo";
+import OrderInfo from "./OrderInfo";
+import ProductInfo from "./ProductInfo";
+import BrandInfo from "./BrandInfo";
+
+export default function () {
+  return (
+    <div className="flex flex-col items-center justify-start w-full h-full">
+      <CardInfo />
+      <OrderInfo />
+      <div className="flex flex-col w-full mt-2 md:flex-row">
+        <ProductInfo />
+        <BrandInfo />
+      </div>
+    </div>
+  );
 }
