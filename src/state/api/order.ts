f18bc28c548ee23937af9862a1ffe5d2a2b001ba
Jay200123 <kenken.ayonon@gmail.com>
2025-01-16 10:56:40 +0800

    Refactor order API calls to use PATH constants for improved maintainability

--- src/state/api/order.ts
+++ src/state/api/order.ts
@@ -1,43 +1,49 @@
 import { OrderState } from "../../interface";
 import { create } from "zustand";
 import api from "../axios";
-
+import { PATH } from "../../constants";
 
 export const useOrderStore = create<OrderState>((set) => ({
   message: "",
   getAllOrders: async () => {
-    const res = await api.get("/orders");
+    const res = await api.get(PATH.ORDERS_ROUTE);
     set({ message: res.data.message });
     return res.data.details;
   },
   getOrderById: async (id) => {
-    const res = await api.get(`/order/${id}`);
+    const res = await api.get(`${PATH.ORDER_ROUTE_ID.replace(":id", id)}`);
     set({ message: res.data.message });
     return res.data.details;
   },
   addOrder: async (values) => {
-    const res = await api.post("/orders", values);
+    const res = await api.post(PATH.ORDERS_ROUTE, values);
     set({ message: res.data.message });
   },
   updateOrderById: async (id, formData) => {
-    const res = await api.put(`/order/edit/${id}`, formData);
+    const res = await api.put(`${PATH.EDIT_ORDER_ROUTE.replace(":id", id)}`, formData);
     set({ message: res.data.message });
   },
   packedOrder: async (id) => {
-    const res = await api.patch(`/order/packed/${id}`);
+    const res = await api.patch(
+      `${PATH.ORDER_PACKED_ROUTE.replace(":id", id)}`
+    );
     set({ message: res.data.message });
   },
   shippedOrder: async (id) => {
-    const res = await api.patch(`/order/shipped/${id}`);
+    const res = await api.patch(
+      `${PATH.ORDER_SHIPPED_ROUTE.replace(":id", id)}`
+    );
     set({ message: res.data.message });
   },
   deliveredOrder: async (id) => {
-    const res = await api.patch(`/order/delivered/${id}`);
+    const res = await api.patch(
+      `${PATH.ORDER_DELIVERED_ROUTE.replace(":id", id)}`
+    );
     set({ message: res.data.message });
   },
 
   deleteOrderById: async (id) => {
-    const res = await api.delete(`/order/delete/${id}`);
+    const res = await api.delete(`${PATH.ORDER_ROUTE_ID.replace(":id", id)}`);
     set({ message: res.data.message });
   },
 }));
