38746bb32f09cf0f4180fa59deafcf7e5a850605
Jay200123 <kenken.ayonon@gmail.com>
2025-01-16 10:52:29 +0800

    Refactor path constants to standardize user and product routes

--- src/constants/path.ts
+++ src/constants/path.ts
@@ -1,6 +1,12 @@
 export const PATH = {
     LOGIN: "/login",	
     USERS_ROUTE: "/users",
-    USER_ROUTE_ID: "/users/:id",
-    EDIT_USER_ROUTE: "/users/edit/:id",
+    USER_ROUTE_ID: "/user/:id",
+    EDIT_USER_ROUTE: "/user/edit/:id",
+    BRANDS_ROUTE: "/brands",    
+    BRAND_ROUTE_ID: "/brand/:id",
+    EDIT_BRAND_ROUTE: "/brand/edit/:id",
+    PRODUCTS_ROUTE: "/products",
+    PRODUCT_ROUTE_ID: "/product/:id",
+    EDIT_PRODUCT_ROUTE: "/product/edit/:id",
 }
