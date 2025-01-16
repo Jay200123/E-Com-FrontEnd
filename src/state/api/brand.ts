4fded9f6f2c14c1df4e9c2b0fe18f890c0895105
Jay200123 <kenken.ayonon@gmail.com>
2025-01-16 10:52:37 +0800

    Refactor brand API calls to use PATH constants for improved maintainability

--- src/state/api/brand.ts
+++ src/state/api/brand.ts
@@ -1,31 +1,34 @@
 import { create } from "zustand";
 import { BrandState } from "../../interface";
 import api from "../axios";
+import { PATH } from "../../constants";
 
 export const useBrandStore = create<BrandState>((set) => ({
   message: "",
   getAllBrands: async () => {
-    const res = await api.get("/brands");
+    const res = await api.get(PATH.BRANDS_ROUTE);
     set({ message: res.data.message });
     return res.data.details;
   },
   getBrandById: async (id: string) => {
-    const res = await api.get(`/brand/${id}`);
+    const res = await api.get(`${PATH.BRAND_ROUTE_ID.replace(":id", id)}`);
     set({ message: res.data.message });
 
     return res.data.details;
   },
 
   addBrand: async (formData) => {
-    const res = await api.post("/brands", formData);
+    const res = await api.post(PATH.BRANDS_ROUTE, formData);
     set({ message: res.data.message });
   },
   updateBrandById: async (id, brand_name) => {
-    const res = await api.put(`/brands/${id}`, { brand_name });
+    const res = await api.put(`${PATH.EDIT_BRAND_ROUTE.replace(":id", id)}`, {
+      brand_name,
+    });
     set({ message: res.data.message });
   },
   deleteBrandById: async (id) => {
-    const res = await api.delete(`/brands/${id}`);
+    const res = await api.delete(`${PATH.BRAND_ROUTE_ID.replace(":id", id)}`);
     set({ message: res.data.message });
   },
 }));
